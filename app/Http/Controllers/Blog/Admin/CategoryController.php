a80e7c72ad761de70be87ebd4187becf4b3af727
Sergey Alekseev <laysikofff@gmail.com>
2020-11-20 16:15:31 +0500

    Lesson 22

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Blog\Admin;
 use App\Models\BlogCategory;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Blog\Admin\BaseController;
+use App\Http\Requests\BlogCategoryUpdateRequest;
 
 class CategoryController extends BaseController
 {
@@ -64,8 +65,29 @@ class CategoryController extends BaseController
      * @param  int  $id
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function update(Request $request, $id)
+    public function update(BlogCategoryUpdateRequest $request, $id)
     {
+        $rules = [
+            'title'       => 'required|min:5|max:200',
+            'slug'        => 'max:200',
+            'description' => 'string|max:500|min:3',
+            'parent_id'   => 'required|integer|exists:blog_categories,id'
+        ];
+
+        //$validateData = $this->validate($request, $rules);
+
+        //$validateData = $request->validate($rules);
+
+//        $validator = \Validator::make($request->all(), $rules);
+//        $validateData[] = $validator->passes();
+//        $validateData[] = $validator->validate();
+//        $validateData[] = $validator->valid();
+//        $validateData[] = $validator->failed();
+//        $validateData[] = $validator->errors();
+//        $validateData[] = $validator->fails();
+//
+//        dd($validateData);
+
         $item = BlogCategory::find($id);
         if (empty($item)) {
             return back()
